0d08a6f798fbc5cdedc4849220eda4fa83e6bffc
Rob Dumas <robdumas@gmail.com>
2017-08-28 22:35:55 -0500

    Converted from tabs to spaces
    Signed-off-by: Rob Dumas <robdumas@gmail.com>

--- lists/views.py
+++ lists/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import redirect, render
 from lists.models import Item
 
 def home_page(request):
-	if request.method == 'POST':
-		Item.objects.create(text=request.POST['item_text'])
-		return redirect('/')
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
 
-	items = Item.objects.all()
-	return render(request, 'home.html', {'items': items})
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
